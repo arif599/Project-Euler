2751294c0a5bab985c0a7b2b5adf1e836ca58316
Arifulla Shaik <arifullashaik01@gmail.com>
2020-10-23 21:00:30 -0700

    Added a new verison and timed it for efficency

--- Problem1.py
+++ Problem1.py
@@ -7,7 +7,35 @@ Find the sum of all the multiples of 3 or 5 below 1000.
 
 ANSWER: 233168
 '''
+#create an effience module in python that takes time as a parameter and calculate the effcience
+#Final Version: 0.0010008811950683594 seconds
+import time
+start_time = time.time()
 
+def main():
+    print(sumOfMultiples(3,5,10))
+    print(sumOfMultiples(3,5,1000))
+
+def sumOfMultiples(mult1, mult2, rangeOfNum):
+    total = 0
+
+    for i in range(1, rangeOfNum):
+        if i%mult1 == 0 or i%mult2 == 0:
+            total += i
+
+    return total
+
+if __name__ == "__main__":
+    main()
+    print(f"--- {time.time() - start_time} seconds ---")
+
+
+import time
+start_time = time.time()
+
+
+'''
+VERSION 1: 0.0071239471435546875 seconds
 def main():
     print(sumOfMultiples(3,5,10))
     print(sumOfMultiples(3,5,1000))
@@ -25,4 +53,5 @@ def sumOfMultiples(mult1, mult2, rangeOfNum):
 
 if __name__ == "__main__":
     main()
-
+    print(f"--- {time.time() - start_time} seconds ---")
+'''
